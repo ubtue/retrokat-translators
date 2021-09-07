f0442f6947f03102b6bf06dc38c7b8f83cd3d353
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-09-07 11:52:10 +0200

    Add review information

--- ubtue_Oxford Academic.js
+++ ubtue_Oxford Academic.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-09-07 08:12:07"
+	"lastUpdated": "2021-09-07 09:51:11"
 }
 
 /*
@@ -68,7 +68,6 @@ function invokeEmbeddedMetadataTranslator(doc, url) {
 		
 		let tagreview = ZU.xpathText(doc, '//*[(@id = "ContentTab")]//a');
 		if (tagreview.match(/Reviews+|Book Reviews+/i)) i.tags.push('Book Review');
-		i.tags.push('Geier');
 		// if the article are review article, then the full text extract is scraped from the HTML
 		let extractText = ZU.xpathText(doc, '//p[@class="chapter-para"]');
 		if (tagreview.match(/Reviews+|Book Reviews+/i) && extractText) i.abstractNote = extractText;
@@ -79,8 +78,8 @@ function invokeEmbeddedMetadataTranslator(doc, url) {
 			let reviewed_year = ZU.xpathText(doc, '//div[@class="product"]/div[@class="year"]');
 			let reviewed_publisher = ZU.xpathText(doc, '//div[@class="product"]/div[@class="publisher-name"]');
 			let reviewed_place = ZU.xpathText(doc, '//div[@class="product"]/div[@class="publisher-loc"]');
-			//i.tags.push('#reviewed_pub#title::' + reviewed_title + '#name::' + reviewed_author_surname + ', ' +  
-			//reviewed_author_given + '#year::' + reviewed_year + '#publisher::' + reviewed_publisher + '#place::' + reviewed_place + '#');
+			i.tags.push('#reviewed_pub#title::' + reviewed_title + '#name::' + reviewed_author_surname + ', ' +  
+			reviewed_author_given + '#year::' + reviewed_year + '#publisher::' + reviewed_publisher + '#place::' + reviewed_place + '#');
 			
 			
 		}
