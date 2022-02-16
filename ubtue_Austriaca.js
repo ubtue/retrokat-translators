d56b70c1616286289db52b3c25dc6d09a9e15c78
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-02-16 10:47:23 +0100

    New translators

---
+++ ubtue_Austriaca.js
@@ -0,0 +1,158 @@
+{
+	"translatorID": "5f4873d0-034c-43ec-95b0-803e5c669bb5",
+	"label": "ubtue_Austriaca",
+	"creator": "Timotheus Kim",
+	"target": "austriaca.at",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 95,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2022-02-16 08:23:07"
+}
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2020 Universitätsbibliothek Tübingen.  All rights reserved.
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+var reviewURLs = [];
+
+function detectWeb(doc, url) {
+	if (url.includes('?arp=')) return "journalArticle";
+	else if (url.match(/inhalt/) && getSearchResults(doc)) return "multiple";
+	else return false;
+}
+
+function getSearchResults(doc, checkOnly) {
+	var items = {};
+	var found = false;
+	var rows = ZU.xpath(doc, '//a');
+	for (let row of rows) {
+		let href = row.href;
+		let title = ZU.trimInternal(row.textContent);
+		if (!href || !title) continue;
+		found = true;
+		if (href.match(/\?arp=/) != null) items[href] = title;
+	}
+	let reviewSection = ZU.xpath(doc, '//div[contains(@class, "issueSubjectGroup")][./h3[@class="issueSubjectGroupHeading"]="II. ABTEILUNG"]');
+	if (reviewSection.length == 0) {
+		reviewSection = ZU.xpath(doc, '//div[contains(@class, "issueSubjectGroup")][./h3[@class="issueSubjectGroupHeading"]="II. Abteilung"]');
+	}
+	var reviewRows = ZU.xpath(reviewSection, './/div[@class="issueArticle"]//a[contains(@class, "issueContentsArticleLink")]');
+	for (let row of reviewRows) {
+		let href = row.href.match(/document/).input;
+		reviewURLs.push(href);
+	}
+	return found ? items : false;
+}
+
+function doWeb(doc, url) {
+	if (detectWeb(doc, url) === "multiple") {
+		Zotero.selectItems(getSearchResults(doc), function (items) {
+			if (!items) {
+				return true;
+			}
+			var articles = [];
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, invokeEMTranslator);
+		});
+	} else {
+		invokeEMTranslator(doc, url);
+	}
+}
+
+function invokeEMTranslator(doc, url) {
+	var translator = Zotero.loadTranslator("web");
+	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
+	translator.setDocument(doc);
+	translator.setHandler("itemDone", function (t, i) {
+		if (i.title.match(/ISBN/)) i.tags.push('Book Review') && delete i.abstractNote;
+		if (i.abstractNote) {
+			i.abstractNote += ZU.xpathText(doc, '//*[(@id = "transAbstract")]//p');
+			if (i.abstractNote.match(/Der Artikel .* wurde .* in der Zeitschrift .* veröffentlicht\.null$/) !== null) {
+				i.abstractNote = '';
+			}
+		}
+		if (i.abstractNote != null) i.abstractNote = i.abstractNote.replace('\.null', '.');
+		if (i.ISSN == "1868-9027") {
+			if (reviewURLs.includes(i.url)) {
+			i.tags.push('Book Review');
+		}
+		}
+		if (ZU.xpathText(doc, '//span[@class="accessOpenAccess mr-2"]') != null) {
+			if (ZU.xpathText(doc, '//span[@class="accessOpenAccess mr-2"]').match(/Open Access/i)) {
+				i.notes.push('LF:');
+			}
+		}
+		let orcidTag = ZU.xpath(doc, '//span[@class="metadataAndContributorsFont"]');
+		if (orcidTag.length != 0) {
+		//Z.debug(orcidTag[0].innerHTML);
+		orcidMatches = orcidTag[0].innerHTML.match(/<span class="contributor"[^>]+>([^<]+)<\/span>\s+<span class="orcidLink"[^>]+><a href="https:\/\/orcid\.org\/([^"]+)"/gi);
+		for (let o in orcidMatches) {
+			let authorTag = orcidMatches[o].match(/<span class="contributor"[^>]+>([^<]+)<\/span>\s+<span class="orcidLink"[^>]+><a href="https:\/\/orcid\.org\/([^"]+)"/i);
+			if (authorTag[2] != undefined) {
+				let author = authorTag[1];
+				let orcid = authorTag[2];
+				i.notes.push({note: "orcid:" + orcid + ' | ' + author});
+			}
+			
+		}
+		}
+		if (i.tags[0] != undefined) {
+			if (i.tags[0] == "journals" || i.tags.length == 0) {
+				i.tags = [];
+			}
+		}
+		if (ZU.xpathText(doc, '//div[contains(., "Keywords:")]')!= null) {
+			KeywordString = ZU.xpathText(doc, '//div[contains(., "Keywords:")]').match(/Keywords:\n(.+?).?Published Online/)[1];
+			for (let keyWordSplitter of [", ", "; ", " - "]) {
+				if (KeywordString.split(keyWordSplitter).length > 2) {
+				for (let keyWord of KeywordString.split(keyWordSplitter)) {
+					i.tags.push(keyWord);
+			}
+			break;
+			}
+			}
+		}
+		if (i.abstractNote != null || i.abstractNote != undefined) {
+			if (i.abstractNote.match(/(^Aus\s+dem\s+Inhalt:\s+)|(^Inhalt:null)/) != null) {
+				i.abstractNote = "";
+			}
+		}
+		let hexCodes = ["&#x2013;", "&#x2018;", "&#x2019;"];
+		let hexCodeToChar = {"&#x2013;":"–", "&#x2018;": "‘", "&#x2019;": "’"};
+		for (let hexCode of hexCodes) {
+			i.title = i.title.replace(hexCode, hexCodeToChar[hexCode]);
+		}
+		let newCreators = [];
+		for (let creator of i.creators) {
+			if (creator.lastName != "BIETAK (Hg.)") newCreators.push(creator);
+		}
+		i.creators = newCreators;
+		i.attachments = [];
+		i.complete();
+	});
+	translator.translate();
+}
---
+++ ubtue_RQS.js
@@ -0,0 +1,160 @@
+{
+	"translatorID": "e6216151-ce62-49cd-97ed-aaee56b533ac",
+	"label": "ubtue_RQS",
+	"creator": "Helena Nebel",
+	"target": "rqs.be\\/app\\/views\\/",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 95,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2022-02-16 09:42:34"
+}
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright © 2019 Simon Kornblith
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
+
+// The variables items and selectArray will be filled during the first
+// as well as the second retrieveDOIs function call and therefore they
+// are defined global.
+
+
+var replaceChars = {"Â¡": "¡", "Â¢": "¢", "Â£": "£", "Â¤": "¤", "Â¥": "¥", "Â¦": "¦", "Â§": "§", "Â¨": "¨", "Â©": "©", "Âª": "ª", "Â«": "«", "Â¬": "¬", "Â®": "®", "Â¯": "¯", "Â°": "°", "Â±": "±", "Â²": "²", "Â³": "³", "Â´": "´", "Âµ": "µ", "Â¶": "¶", "Â·": "·", "Â¸": "¸", "Â¹": "¹", "Âº": "º", "Â»": "»", "Â¼": "¼", "Â½": "½", "Â¾": "¾", "Â¿": "¿", "Ã€": "À", "Ã": "Á", "Ã‚": "Â", "Ãƒ": "Ã", "Ã„": "Ä", "Ã…": "Å", "Ã†": "Æ", "Ã‡": "Ç", "Ãˆ": "È", "Ã‰": "É", "ÃŠ": "Ê", "Ã‹": "Ë", "ÃŒ": "Ì", "Ã": "Í", "ÃŽ": "Î", "Ã": "Ï", "Ã": "Ð", "Ã‘": "Ñ", "Ã’": "Ò", "Ã“": "Ó", "Ã”": "Ô", "Ã•": "Õ", "Ã–": "Ö", "Ã—": "×", "Ã˜": "Ø", "Ã™": "Ù", "Ãš": "Ú", "Ã›": "Û", "Ãœ": "Ü", "Ã": "Ý", "Ãž": "Þ", "ÃŸ": "ß", "Ã¡": "á", "Ã¢": "â", "Ã£": "ã", "Ã¤": "ä", "Ã¥": "å", "Ã¦": "æ", "Ã§": "ç", "Ã¨": "è", "Ã©": "é", "Ãª": "ê", "Ã«": "ë", "Ã¬": "ì", "Ã­": "í", "Ã®": "î", "Ã¯": "ï", "Ã°": "ð", "Ã±": "ñ", "Ã²": "ò", "Ã³": "ó", "Ã´": "ô", "Ãµ": "õ", "Ã¶": "ö", "Ã·": "÷", "Ã¸": "ø", "Ã¹": "ù", "Ãº": "ú", "Ã»": "û", "Ã¼": "ü", "Ã½": "ý", "Ã¾": "þ", "Ã¿": "ÿ", "Ã": "à"};
+
+var articleInfo = {};
+var volume = "";
+var issue = "";
+var year = "";
+var issn = "";
+
+function detectWeb(doc, url) {
+	if (getSearchResults(doc, url, true)) {
+		return "multiple";
+	}
+}
+
+function getSearchResults(doc, url, checkOnly) {
+	var items = {};
+	var found = false;
+	let html = doc.body.innerHTML;
+	html = html.replace(/\n/g, "");
+	let volumeInformation = html.match(/<div class="w3-col m2 w3-section">.+?<span class="w3-xlarge">(.+?)<\/span><br>.+?<\/div>/g);
+	for (let infoTag of volumeInformation) {
+		let info = infoTag.match(/<div class="w3-col m2 w3-section">.+?<span class="w3-xlarge">(.+?)<\/span><br>\s+(.+?)\s+?<\/div>/);
+		if (info != null) {
+		if (info[2] == "Année") {
+			year = info[1];
+			}
+		if (info[2] == "Tome") {
+		volume = info[1];
+		}
+		if (info[2] == "Numéro") {
+			issue = info[1];
+		}
+		}
+	}
+	let articleNum = 0;
+	for (let articleTag of ZU.xpath(doc, '//p[@class="pub-header w3-padding w3-dark-grey w3-hover-light-gray w3-block w3-left-align"]')) {
+		items[url + "===" + articleNum] = articleTag.textContent;
+		found = true;
+		articleInfo[url + "===" + articleNum] = articleTag.innerHTML;
+		articleNum += 1;
+	}
+	return found ? items : false;
+}
+
+function doWeb(doc, url) {
+		Zotero.selectItems(getSearchResults(doc, url, false), function (items) {
+			if (!items) {
+				return true;
+			}
+			var articles = [];
+			for (var i in items) {
+				articles.push(i);
+			}
+			ZU.processDocuments(articles, scrape);
+		});
+}
+
+function scrape(doc, url) {
+	article = articleInfo[url];
+	let item = new Zotero.Item("journalArticle");
+	Z.debug(article);
+		let articleTitle = article.match(/<u>(.+?)<\/u>/);
+		if (articleTitle != null) {
+			item.title = articleTitle[1].replace(/<\/?.+?>/g, '');
+		}
+		let articleCreators = article.match(/<span class="text-uppercase font-weight-light">(.+?)<\/span>/);
+		if (articleCreators != null) {
+			for (let articleCreator of articleCreators[1].split(", ")) {
+				let newCreator = articleCreator.match(/^(.+)\s+\((.+?)\)/);
+				if (newCreator != null) {
+					let firstName = newCreator[2];
+					let lastName = newCreator[1];
+					item.creators.push({"firstName": firstName, "lastName": lastName, "creatorType": "author"});
+			}
+			}
+		}
+		let pagination = article.match(/p.\s?(\d+(?:-\d+)?)/);
+			if (pagination != null) {
+				item["pages"] = pagination[1];
+		}
+		item.url = url.replace(/===\d+$/, "");
+		item.issue = issue;
+		item.volume = volume;
+		item.date = year;
+		item.ISSN = issn;
+		item.complete();
+}
+
+
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://blog.apastyle.org/apastyle/digital-object-identifier-doi/",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://libguides.csuchico.edu/citingbusiness",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://www.egms.de/static/de/journals/mbi/2015-15/mbi000336.shtml",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://www.roboticsproceedings.org/rss09/p23.html",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://en.wikipedia.org/wiki/Template_talk:Doi",
+		"items": "multiple"
+	}
+]
+/** END TEST CASES **/
