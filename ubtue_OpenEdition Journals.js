d98900db5ee90eb727263b18f56dddc1f5f1b330
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-03-03 15:40:11 +0100

    Add review tags

--- ubtue_OpenEdition Journals.js
+++ ubtue_OpenEdition Journals.js
@@ -1,136 +1,378 @@
 {
-	"translatorID": "55275811-58f4-4f5e-b711-a043f1fc50da",
+	"translatorID": "2c5eb32f-ba13-4f56-a231-62eb8077cb44",
 	"label": "ubtue_OpenEdition Journals",
-	"creator": "Madeesh Kannan",
-	"target": "https?://journals.openedition.org",
+	"creator": "Aurimas Vinckevicius, Pierre-Alain Mignot, Michael Berkowitz, Hélène Prieto, Jean-François Rivière",
+	"target": "^https?://journals\\.openedition\\.org/",
 	"minVersion": "3.0",
 	"maxVersion": "",
-	"priority": 150,
-	"inRepository": false,
+	"priority": 99,
+	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-06-23 12:51:51"
+	"lastUpdated": "2022-03-03 14:37:32"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2019 Universitätsbibliothek Tübingen All rights reserved.
+	Copyright © 2020-2021 Aurimas Vinckevicius, Pierre-Alain Mignot, Michael Berkowitz, Hélène Prieto, Jean-François Rivière
 
-	This program is free software: you can redistribute it and/or modify
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
+	Zotero is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU Affero General Public License for more details.
 
 	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
 
 	***** END LICENSE BLOCK *****
 */
 
+function detectWeb(doc, url) {
+	var types = ZU.xpath(doc, '//meta[@name="DC.type"]/@content');
+	for (let i = 0, n = types.length; i < n; i++) {
+		switch (types[i].textContent.toLowerCase()) {
+			case 'journalarticle':
+				return 'journalArticle';
+			case 'collection':
+				return 'multiple';
+			case 'booksection':
+				return 'bookSection';
+		}
+	}
 
-function detectWeb(doc) {
-	if (getSearchResults(doc)) return "multiple";
-	else if (ZU.xpath(doc, '//h1[@id="docTitle"]').length === 1) {
+	if (ZU.xpath(doc, '//div[@id="inside"]/div[@class="sommaire"]/dl[@class="documents"]/dd[@class="titre"]/a').length
+		|| ZU.xpath(doc, '//ul[@class="summary"]//div[@class="title"]/a').length) {
+		return "multiple";
+	}
+	else if (ZU.xpath(doc, '//h1[@id="docTitle"]/span[@class="text"]').length
+		|| /document\d+/.test(url)) {
 		return "journalArticle";
 	}
 	return false;
 }
 
-function getSearchResults(doc) {
-	var items = {};
-	var found = false;
-	var rows = ZU.xpath(doc, '//li[contains(@class,"textes")]//div[@class="title"]//a');
-	for (let i = 0; i < rows.length; i++) {
-		let href = rows[i].href;
-		let title = ZU.trimInternal(rows[i].textContent);
-		if (!href || !title) continue;
-		found = true;
-		items[href] = title;
+function scrape(doc, url) {
+	// Example: https://journals.openedition.org/remi/persee-144614
+	if (/persee-\d+/.test(url)) {
+		// the article is on Persée portal, getting it to be translated by COinS
+		let translator = Zotero.loadTranslator("web");
+		translator.setTranslator("05d07af9-105a-4572-99f6-a8e231c0daef");
+		translator.setDocument(doc);
+		translator.translate();
 	}
-	return found ? items : false;
-}
-
-function invokeEmbeddedMetadataTranslator(doc) {
-	let translator = Zotero.loadTranslator("web");
-	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
-	translator.setDocument(doc);
-	translator.setHandler("itemDone", function (t, item) {
-		let abstracts = ZU.xpath(doc, '//p[@class="resume"]');
-		if (abstracts) {
-			abstracts = abstracts.map(x => x.textContent.trim());
-			for (let i = 0; i < abstracts.length; ++i) {
-				if (i == 0) item.abstractNote = abstracts[i];
-				else item.notes.push({ note: "abs:" + abstracts[i] });
+	else {
+		// use Embeded Metadata
+		let translator = Zotero.loadTranslator('web');
+		translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
+		translator.setDocument(doc);
+		translator.setHandler('itemDone', function (obj, item) {
+			if (ZU.xpathText(doc, '//div[@id="docSubParent"]//span[@class="title"]') != null) {
+				if (ZU.xpathText(doc, '//div[@id="docSubParent"]//span[@class="title"]').match(/(Recensions)|(Bibliographie)/) != null) {
+					item.tags.push("Book Review");
+					if (ZU.xpathText(doc, '//p[@class="noticebibliooeuvre"]') != null) {
+						let reviewed_title = ZU.xpathText(doc, '//p[@class="noticebibliooeuvre"]');
+						let isbn = reviewed_title.match(/ISBN\s*:?\s+([\d-]+)/);
+						if (isbn != null) {
+							item.tags.push("#reviewed_pub#isbn∷" + isbn[1] + "#");
+						}
+					}
+				}
 			}
-		}
-
-		item.tags = ZU.xpath(doc, '//div[@id="entries"]//div[@class="index ltr"]//a | //div[@id="entries"]//div[@class="index"]//a').map(x => x.textContent.trim());
-		if (item.issue) {
-			let issueAndVol = item.issue.match(/(\d+)\/(\d+)/);
-			if (issueAndVol) {
-				item.volume = issueAndVol[1];
-				item.issue = issueAndVol[2];
+			// editor and translator declarations not part of DC spec
+			// editors (and compilers)
+			var editors = ZU.xpath(doc, '//meta[@name="DC.contributor.edt" or @name="DC.contributor.com"]/@content');
+			for (let i = 0, n = editors.length; i < n; i++) {
+				item.creators.push(
+					ZU.cleanAuthor(editors[i].textContent, 'editor', true));
 			}
-		}
+			// translators
+			var trans = ZU.xpath(doc,
+				'//meta[@name="DC.contributor.trl"]/@content');
+			for (let i = 0, n = trans.length; i < n; i++) {
+				item.creators.push(
+					ZU.cleanAuthor(trans[i].textContent, 'translator', true));
+			}
+			// fix all caps for author last names
+			for (let i = 0; i < item.creators.length; i++) {
+				if (item.creators[i].lastName == item.creators[i].lastName.toUpperCase()) {
+					item.creators[i].lastName = ZU.capitalizeTitle(item.creators[i].lastName.toLowerCase(), true);
+				}
+			}
+			// set abstract and keywords based on documents language
+			var locale = ZU.xpathText(doc, '//meta[@name="citation_language"]/@content');
+			// default to french if not set
+			locale = locale ? locale.toLowerCase() : 'fr';
 
-		let section = ZU.xpathText(doc, '//div[contains(@class, "souspartie")]//span[@class="title"]');
-		if (section && section.match(/(Recensions|Notes de lecture)/)) item.tags.push("Book Review");
-		item.itemType = "journalArticle";
-		item.complete();
-	});
-	translator.translate();
+			// get abstract and tags in document language
+			// or the first locale available
+			item.abstractNote = ZU.xpathText(doc,
+				'//meta[@name="description" or @name="DC.description"][lang("' + locale + '") or @lang="' + locale + '"][1]/@content')
+				|| ZU.xpathText(doc,
+					'//meta[@name="description" or @name="DC.description"][1]/@content');
+
+			var tags = ZU.xpathText(doc,
+				'//meta[@name="keywords" or @name="DC.subject"][lang("' + locale + '") or @lang="' + locale + '"][1]/@content')
+				|| ZU.xpathText(doc,
+					'//meta[@name="keywords" or @name="DC.subject"][1]/@content');
+			if (tags) {
+				item.tags = tags.trim().split(/\s*,\s*/);
+			}
+
+			delete item.extra;
+		
+			// The site lists all editor of journals as editor in the header. Remove them.
+			// I don't think there is a use case for editors for journal articles
+			if (item.itemType === "journalArticle") {
+				for (let i = 0; i < item.creators.length; i++) {
+					if (item.creators[i].creatorType === "editor") {
+						item.creators.splice(i--, 1);
+					}
+				}
+			}
+			
+			item.attachments = [];
+			
+			// store the language-specific url
+			item.url = url;
+
+			item.complete();
+		});
+
+		translator.getTranslatorObject(function (trans) {
+			// override some of the mappings
+			trans.addCustomFields({
+				'prism.number': 'issue',
+				'prism.volume': 'volume',
+				'DC.title': 'title'
+			});
+
+			trans.doWeb(doc, url);
+		});
+	}
 }
 
 function doWeb(doc, url) {
-	if (detectWeb(doc, url) === "multiple") {
-		Zotero.selectItems(getSearchResults(doc), function (items) {
-			if (!items) {
-				return true;
+	if (detectWeb(doc, url) == "multiple") {
+		var results = ZU.xpath(doc, '//div[@id="inside"]/div[@class="sommaire"]/dl[@class="documents"]/dd[@class="titre"]');
+		if (!results.length) {
+			results = ZU.xpath(doc, '//ul[@class="summary"]//div[@class="title"]');
+		}
+
+		Zotero.selectItems(ZU.getItemArray(doc, results), function (selectedItems) {
+			if (!selectedItems) return true;
+
+			var urls = [];
+			for (let i in selectedItems) {
+				urls.push(i);
 			}
-			let articles = [];
-			for (let i in items) {
-				articles.push(i);
-			}
-			ZU.processDocuments(articles, invokeEmbeddedMetadataTranslator);
+
+			ZU.processDocuments(urls, scrape);
+
+			return true;
 		});
-	} else invokeEmbeddedMetadataTranslator(doc, url);
+	}
+	else {
+		scrape(doc, url);
+	}
 }
+
 /** BEGIN TEST CASES **/
 var testCases = [
 	{
 		"type": "web",
-		"url": "https://journals.openedition.org/rsr/4866",
+		"url": "https://journals.openedition.org/amerika/1283",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://journals.openedition.org/e-spania/12303?lang=fr",
 		"items": [
 			{
 				"itemType": "journalArticle",
-				"title": "« Poésie, Bible et théologie de l’Antiquité tardive au Moyen Âge » (Strasbourg, 25‑27 janv. 2018)",
+				"title": "Le testament d’Elvire (Tábara, 1099)",
 				"creators": [
 					{
-						"firstName": "Michele",
-						"lastName": "Cutino",
+						"firstName": "Georges",
+						"lastName": "Martin",
 						"creatorType": "author"
 					}
 				],
-				"date": "2018-04-01T00:00:00+02:00",
-				"ISSN": "0035-2217",
-				"abstractNote": "Du 25 au 27 janvier 2018 s’est déroulé à Strasbourg un colloque international intitulé « Poésie, Bible et théologie de l’Antiquité tardive au Moyen Âge », qui était organisé par la Faculté de théologie catholique avec le soutien de l’Université de Strasbourg et en collaboration avec plusieurs institutions nationales (Institut d’Études Augustiniennes [IEA] ; Laboratoire d’études sur les monothéismes [LEM – UMR 8584] ; Centre Jean Mabillon (EA 3624), École nationale des chartes ; Association « ...",
-				"issue": "2",
+				"date": "2008/02/01",
+				"DOI": "10.4000/e-spania.12303",
+				"ISSN": "1951-6169",
+				"abstractNote": "Le testament d’Elvire livre de précieuses informations sur la réalité historique de l’infantat : son implantation, la composition de ses biens, ses évolutions, les formes de son acquisition et de sa transmission, sa fonction politique. Mais il nous renseigne aussi sur une infante de niveau moyen, sur son cadre de vie, son entourage, ses activités, les réseaux de son pouvoir et même sur sa foi.",
+				"issue": "5",
 				"language": "fr",
 				"libraryCatalog": "journals.openedition.org",
-				"pages": "277-278",
-				"publicationTitle": "Revue des sciences religieuses",
-				"rights": "© RSR",
-				"url": "http://journals.openedition.org/rsr/4866",
-				"volume": "92",
+				"publicationTitle": "e-Spania. Revue interdisciplinaire d’études hispaniques médiévales et modernes",
+				"rights": "Les contenus de la revue e-Spania sont mis à disposition selon les termes de la Licence Creative Commons Attribution - Pas d'Utilisation Commerciale - Pas de Modification 4.0 International.",
+				"url": "https://journals.openedition.org/e-spania/12303?lang=fr",
 				"attachments": [
 					{
-						"title": "Snapshot"
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Alphonse VI de Castille et de León"
+					},
+					{
+						"tag": "Elvire Fernandez"
+					},
+					{
+						"tag": "Ferdinand Ier de Castille et de León"
+					},
+					{
+						"tag": "Saint-Isidore de León"
+					},
+					{
+						"tag": "Sancie Raimundez"
+					},
+					{
+						"tag": "Urraque Fernandez"
+					},
+					{
+						"tag": "XIe siècle"
+					},
+					{
+						"tag": "infantat"
+					},
+					{
+						"tag": "infantaticum"
+					},
+					{
+						"tag": "infante Elvire"
+					},
+					{
+						"tag": "infante Sancie"
+					},
+					{
+						"tag": "infante Urraque"
+					},
+					{
+						"tag": "testament"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://journals.openedition.org/e-spania/12303?lang=es",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Le testament d’Elvire (Tábara, 1099)",
+				"creators": [
+					{
+						"firstName": "Georges",
+						"lastName": "Martin",
+						"creatorType": "author"
+					}
+				],
+				"date": "2008/02/01",
+				"DOI": "10.4000/e-spania.12303",
+				"ISSN": "1951-6169",
+				"abstractNote": "Le testament d’Elvire livre de précieuses informations sur la réalité historique de l’infantat : son implantation, la composition de ses biens, ses évolutions, les formes de son acquisition et de sa transmission, sa fonction politique. Mais il nous renseigne aussi sur une infante de niveau moyen, sur son cadre de vie, son entourage, ses activités, les réseaux de son pouvoir et même sur sa foi.",
+				"issue": "5",
+				"language": "fr",
+				"libraryCatalog": "journals.openedition.org",
+				"publicationTitle": "e-Spania. Revue interdisciplinaire d’études hispaniques médiévales et modernes",
+				"rights": "Les contenus de la revue e-Spania sont mis à disposition selon les termes de la Licence Creative Commons Attribution - Pas d'Utilisation Commerciale - Pas de Modification 4.0 International.",
+				"url": "https://journals.openedition.org/e-spania/12303?lang=es",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Alphonse VI de Castille et de León"
+					},
+					{
+						"tag": "Elvire Fernandez"
+					},
+					{
+						"tag": "Ferdinand Ier de Castille et de León"
+					},
+					{
+						"tag": "Saint-Isidore de León"
+					},
+					{
+						"tag": "Sancie Raimundez"
+					},
+					{
+						"tag": "Urraque Fernandez"
+					},
+					{
+						"tag": "XIe siècle"
+					},
+					{
+						"tag": "infantat"
+					},
+					{
+						"tag": "infantaticum"
+					},
+					{
+						"tag": "infante Elvire"
+					},
+					{
+						"tag": "infante Sancie"
+					},
+					{
+						"tag": "infante Urraque"
+					},
+					{
+						"tag": "testament"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://journals.openedition.org/chs/142",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "L’encadrement des Algériens de Paris (1944-1954), entre contraintes juridiques et arbitraire policier",
+				"creators": [
+					{
+						"firstName": "Emmanuel",
+						"lastName": "Blanchard",
+						"creatorType": "author"
+					}
+				],
+				"date": "2007/06/01",
+				"DOI": "10.4000/chs.142",
+				"ISSN": "1422-0857",
+				"abstractNote": "Au sortir de la Seconde Guerre mondiale, pour sauvegarder son empire colonial, la France est contrainte de reconnaître la citoyenneté des Français musulmans d’Algérie (FMA). Dès lors, ceux-ci se retrouvent en métropole dans une situation proche de celle d’autres citoyens diminués (vagabonds, prostituées…) qui, bien que juridiquement peu accessibles à la répression policière sont considérés comme « indésirables » et constituent la clientèle privilégiée de forces de l’ordre agissant aux marges de la loi. Si l’ethnicité, la xénophobie, et la situation coloniale contribuent à définir les Algériens comme « indésirables », le répertoire d’actions policier envers les FMA tient avant tout à la façon dont l’arène policière est médiatisée par le contrôle et la représentation politiques.",
+				"issue": "1",
+				"language": "fr",
+				"libraryCatalog": "journals.openedition.org",
+				"pages": "5-25",
+				"publicationTitle": "Crime, Histoire & Sociétés / Crime, History & Societies",
+				"rights": "© Droz",
+				"url": "https://journals.openedition.org/chs/142",
+				"volume": "11",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
 					}
 				],
 				"tags": [],
@@ -141,35 +383,53 @@ var testCases = [
 	},
 	{
 		"type": "web",
-		"url": "https://journals.openedition.org/rsr/4866",
+		"url": "https://journals.openedition.org/remi/2495",
 		"items": [
 			{
 				"itemType": "journalArticle",
-				"title": "« Poésie, Bible et théologie de l’Antiquité tardive au Moyen Âge » (Strasbourg, 25‑27 janv. 2018)",
+				"title": "Multiracial Classification on the United States Census",
 				"creators": [
 					{
-						"firstName": "Michele",
-						"lastName": "Cutino",
+						"firstName": "Ann",
+						"lastName": "Morning",
 						"creatorType": "author"
 					}
 				],
-				"date": "2018-04-01T00:00:00+02:00",
-				"ISSN": "0035-2217",
-				"abstractNote": "Du 25 au 27 janvier 2018 s’est déroulé à Strasbourg un colloque international intitulé « Poésie, Bible et théologie de l’Antiquité tardive au Moyen Âge », qui était organisé par la Faculté de théologie catholique avec le soutien de l’Université de Strasbourg et en collaboration avec plusieurs institutions nationales (Institut d’Études Augustiniennes [IEA] ; Laboratoire d’études sur les monothéismes [LEM – UMR 8584] ; Centre Jean Mabillon (EA 3624), École nationale des chartes ; Association « ...",
+				"date": "2005/06/01",
+				"DOI": "10.4000/remi.2495",
+				"ISSN": "0765-0752",
+				"abstractNote": "In 1997, the United States’ federal guidelines on racial classification were amended to permit individual respondents to identify themselves as members of more than one race. This measure, taken at the urging of a vocal community of mixed-race individuals and organizations, was seen by many as having important consequences. In this article I examine the predictions about the impact of multiple-race classification, and assess how accurate they have proved to be. I conclude however that neither the hopes nor fears associated with multiracial recognition have been realized. Instead, the most important legacy of the recognition of mixed-race America is likely to be its contribution to the debate about classifying a much larger segment of the population: the Hispanic community.",
 				"issue": "2",
-				"language": "fr",
+				"language": "en",
 				"libraryCatalog": "journals.openedition.org",
-				"pages": "277-278",
-				"publicationTitle": "Revue des sciences religieuses",
-				"rights": "© RSR",
-				"url": "http://journals.openedition.org/rsr/4866",
-				"volume": "92",
+				"pages": "111-134",
+				"publicationTitle": "Revue européenne des migrations internationales",
+				"rights": "© Université de Poitiers",
+				"url": "https://journals.openedition.org/remi/2495",
+				"volume": "21",
 				"attachments": [
 					{
-						"title": "Snapshot"
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
 					}
 				],
-				"tags": [],
+				"tags": [
+					{
+						"tag": "ethnicisation"
+					},
+					{
+						"tag": "méthodologie"
+					},
+					{
+						"tag": "recensement"
+					},
+					{
+						"tag": "statistiques"
+					},
+					{
+						"tag": "États-Unis"
+					}
+				],
 				"notes": [],
 				"seeAlso": []
 			}
